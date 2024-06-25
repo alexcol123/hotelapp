50bfc0a04ef374b9251a732e23a95d2a3f58db96
Henry Munoz <alexcol123@yahoo.com>
2024-06-25 15:16:35 -0400

    fix navsearch component

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,13 +1,13 @@
 'use client'
 import { Input } from '../ui/input'
-import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useSearchParams, useRouter } from 'next/navigation'
 import { useDebouncedCallback } from 'use-debounce'
 import { useState, useEffect } from 'react'
-import { set } from 'date-fns'
+
 
 const NavSearch = () => {
   const searchParams = useSearchParams()
-  const pathname = usePathname()
+  
   const { replace } = useRouter()
 
   const [search, setsearch] = useState(searchParams.get('search')?.toString() || '')
@@ -21,7 +21,7 @@ const NavSearch = () => {
     } else {
       params.delete('search')
     }
-    replace(`${pathname}?${params.toString()}`)
+    replace(`/?${params.toString()}`)
   }, 500)
 
 
