a192694c595d9b9b6d5b705df06a8637f00d501f
Henry Munoz <alexcol123@yahoo.com>
2024-06-28 09:40:38 -0400

    feat: Update ReviewsPage component to display user reviews and allow deleting reviews
    - Import necessary components and actions for displaying and deleting reviews
    - Fetch user reviews using fetchPropertyReviewsByUser function
    - Display a message if the user has not submitted any reviews yet
    - Render ReviewCard components for each review, passing necessary props
    - Add DeleteReview component for deleting reviews, with deleteReviewAction function
    - Update exports to include DeleteReview component

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button'
 
 import { SignInButton } from '@clerk/nextjs'
 import { FaRegHeart, FaHeart } from 'react-icons/fa'
-
+import { LuTrash2, LuPenSquare } from 'react-icons/lu'
 
 type btnSize = 'default' | 'sm' | 'lg'
 type SubmitButtonProps = {
@@ -15,8 +15,6 @@ type SubmitButtonProps = {
   text?: string,
   size?: btnSize
 }
-
-
 
 export const SubmitButton = ({ className = '', text = 'submit', size = 'lg' }: SubmitButtonProps) => {
 
@@ -61,4 +59,34 @@ export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
         : <FaRegHeart />}
   </Button>
 
+}
+
+
+type actionType = 'edit' | 'delete'
+
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus()
+
+  const renderIcon = () => {
+    switch (actionType) {
+      case 'edit':
+        return <LuPenSquare />
+      case 'delete':
+        return <LuTrash2 />
+      default:
+        const never: never = actionType
+        throw new Error(`Invalid action type: ${never}`)
+    }
+  }
+
+  return (
+    <Button
+      type='submit'
+      size='icon'
+      variant='link'
+      className='p-2 cursor-pointer'
+    >
+      {pending ? <ReloadIcon className=' animate-spin' /> : renderIcon()}
+    </Button>
+  )
 }
